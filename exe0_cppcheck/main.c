794ae2ca2f238534f5bf0538b5bbe1cc262d2861
laracmv <cmvlara1@gmail.com>
2026-02-20 09:31:38 -0300

    ex0

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -12,10 +12,12 @@ int main(void) {
     int a = 5;
     int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
+    printf("The product of %d and %d is: %d\n", a, b, result);
 
     int arr[NUM_ELEMENTS];
     for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        printf("Element %d: %d\n", i, arr[i]);
     }
 
     return 0;
