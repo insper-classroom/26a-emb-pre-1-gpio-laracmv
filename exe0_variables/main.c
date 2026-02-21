3a765ed4272686cd231ce68c7423fcabe1b6e491
laracmv <cmvlara1@gmail.com>
2026-02-21 18:30:28 -0300

    ex0

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -7,13 +7,13 @@ void foo(int *a) {
 
 void main(void) {
     int a = 0; 
-    int b = 0;
     
     while (1) {
         foo(&a);
         
         if (a > 5) {
-            b = 1;
+            int b = 1;
+
         }
     }
 }
