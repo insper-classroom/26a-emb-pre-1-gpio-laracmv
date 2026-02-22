31291f2697c662a0164828861ae7feb540211f74
laracmv <cmvlara1@gmail.com>
2026-02-22 14:40:29 -0300

    ex6

--- exe6/main.c
+++ exe6/main.c
@@ -7,7 +7,6 @@ int FIRST_GPIO = 2;
 const int BTN_PIN_G = 28;
 
 
-
 // This array converts a number 0-9 to a bit pattern to send to the GPIOs
 int bits[10] = {
     0x3f,  // 0
@@ -39,14 +38,11 @@ void seven_seg_display(int cnt) {
 }
 
 int main() {
-    // int BUTTON_GPIO;
     int cnt = 0;
     int last_btn = 1; // Button not pressed (pulled up)
 
     stdio_init_all();
     
-
-    // BUTTON_GPIO = FIRST_GPIO + 7;
 
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
