d6f6a99d9d7357540d1882af56a7cc7ac0d063db
laracmv <cmvlara1@gmail.com>
2026-02-22 14:14:48 -0300

    ex6

--- exe6/main.c
+++ exe6/main.c
@@ -6,8 +6,6 @@
 int FIRST_GPIO = 2;
 const int BTN_PIN_G = 28;
 
-int BUTTON_GPIO;
-int cnt;
 int last_btn; // Button not pressed (pulled up)
 
 // This array converts a number 0-9 to a bit pattern to send to the GPIOs
@@ -31,8 +29,8 @@ void seven_seg_init() {
     }
 }
 
-void seven_seg_display() {
-    int value = bits[cnt];
+void seven_seg_display(int *cnt) {
+    int value = bits[*cnt];
     for (int i = 0; i < 7; i++) {
         int gpio = FIRST_GPIO + i;
         int bit = (value >> i) & 1;
@@ -41,16 +39,20 @@ void seven_seg_display() {
 }
 
 int main() {
-    stdio_init_all();
+    // int BUTTON_GPIO;
+    int cnt;
 
-    BUTTON_GPIO = FIRST_GPIO + 7;
+    stdio_init_all();
+    
+
+    // BUTTON_GPIO = FIRST_GPIO + 7;
 
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
 
     seven_seg_init();
-    seven_seg_display(2);
+    seven_seg_display(&cnt);
 
     while (true) {
         int btn = gpio_get(BTN_PIN_G);
@@ -58,7 +60,7 @@ int main() {
             if (++cnt > 9) {
                 cnt = 0;
             }
-            seven_seg_display();
+            seven_seg_display(&cnt);
             printf("cnt: %d\n", cnt);
         }
         last_btn = btn;
